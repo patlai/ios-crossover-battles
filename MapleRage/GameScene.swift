2ec1ebff844e9b742f6e9311460d7a1bfaf55f71
Patrick Lai <patricklai10123@gmail.com>
2019-01-27 14:29:15 -0500

    add hit detection

--- MapleRage/GameScene.swift
+++ MapleRage/GameScene.swift
@@ -1,38 +1,5 @@
 import SpriteKit
 
-class Monster{
-    var MaxHP: Double
-    var CurrentHP: Double
-    var Node: SKSpriteNode
-    var DefaultAnimation: SKAction
-    var DamageAnimation: SKAction
-    var DeathAnimation: SKAction
-    var DamageSound: SKAction
-    var DeathSound: SKAction
-    
-    init(){
-        self.MaxHP = 0
-        self.CurrentHP = 0
-        self.DefaultAnimation = SKAction()
-        self.DamageAnimation = SKAction()
-        self.DeathAnimation = SKAction()
-        self.DamageSound = SKAction()
-        self.DeathSound = SKAction()
-        self.Node = SKSpriteNode()
-    }
-    
-    init (_ MaxHP: Double, _ StartingFrame: String, _ DefaultAnimation: SKAction, _ DamageAnimation: SKAction, _ DeathAnimation: SKAction, _ DamageSound: SKAction, _ DeathSound: SKAction){
-        self.MaxHP = MaxHP
-        self.CurrentHP = MaxHP
-        self.DefaultAnimation = DefaultAnimation
-        self.DamageAnimation = DamageAnimation
-        self.DeathAnimation = DeathAnimation
-        self.DamageSound = DamageSound
-        self.DeathSound = DeathSound
-        self.Node = SKSpriteNode(imageNamed: StartingFrame)
-    }
-}
-
 class GameScene: SKScene {
     var monsters: [Monster] = Array()
     var currentMonster = Monster()
@@ -48,26 +15,9 @@ class GameScene: SKScene {
     let maxDamage = 99999.0
     let minDamage = 10000.0
     let spriteFrameCount = 6
+    let defaultFontSize = 45.0
     
     var damageSound = SKAction.playSoundFileNamed("sound/mushroom_damage.mp3", waitForCompletion: false)
-    
-    func getDefaultAnimation(_ prefix: String, _ suffix: String, _ numberOfFrames: Int, _ frameDuration: Double) -> SKAction{
-        var frames: [SKTexture] = Array()
-        for i in 0 ..< numberOfFrames {
-            let current = SKTexture.init(imageNamed: prefix + String(i) + suffix)
-            frames.append(current)
-        }
-        
-        return SKAction.animate(with: frames, timePerFrame: frameDuration)
-    }
-    
-    func getSpecialAnimation(_ spritePath: String, _ frameDuration: Double) -> SKAction {
-        return SKAction.animate(with: Array([SKTexture.init(imageNamed: spritePath)]), timePerFrame: frameDuration )
-    }
-    
-    func getSoundClip(_ soundFilePath: String, _ wait: Bool = false) -> SKAction {
-        return SKAction.playSoundFileNamed(soundFilePath, waitForCompletion: wait)
-    }
     
     func updateMonsterHPLabel(_ label: SKLabelNode, _ monster: Monster){
         label.text = String(monster.CurrentHP) + " / " + String(monster.MaxHP)
@@ -100,27 +50,18 @@ class GameScene: SKScene {
             y: 5 * view.frame.height / 6
         )
         
-        var mushmom = Monster(
+        let mushmom = Monster(
             1000000.0,
             "mushroom/f0.png",
-            getDefaultAnimation("mushroom/f", ".png", 6, 0.15),
-            getSpecialAnimation("mushroom/f_hit.png", 0.3),
-            getSpecialAnimation("mushroom/f_hit.png", 0.3),
-            getSoundClip("sound/mushroom_damage.mp3"),
-            getSoundClip("sound/mushroom_death.mp3")
+            Monster.getDefaultAnimation("mushroom/f", ".png", 6, 0.15),
+            Monster.getSpecialAnimation("mushroom/f_hit.png", 0.3),
+            Monster.getSpecialAnimation("mushroom/f_hit.png", 0.3),
+            Monster.getSoundClip("sound/mushroom_damage.mp3"),
+            Monster.getSoundClip("sound/mushroom_death.mp3")
         )
-        mushmom.Node.position = center
-        monsters.append(mushmom)
         
-//        var frames: [SKTexture] = Array()
-//        for i in 0 ..< spriteFrameCount {
-//            let current = SKTexture.init(imageNamed: "mushroom/f" + String(i) + ".png")
-//            frames.append(current)
-//        }
-//
-//        defaultMonsterAnimation = SKAction.animate(with: frames, timePerFrame: 0.15)
-//        monsterNode.position = center
-//        monsterNode.run(SKAction.repeatForever(defaultMonsterAnimation))
+        mushmom.Position = center
+        monsters.append(mushmom)
      
         currentMonster = monsters[0]
         self.addChild(currentMonster.Node)
@@ -141,56 +82,68 @@ class GameScene: SKScene {
         view.addGestureRecognizer(recognizer)
     }
     
-    @objc func tap (recognizer: UIGestureRecognizer){
-        let viewLocation = recognizer.location(in: view)
-        let tapLocation = convertPoint(fromView: viewLocation)
-        
-        var damage = Double.random(in: minDamage ... maxDamage)
-        damage.round()
-        currentMonster.CurrentHP = max(0, currentMonster.CurrentHP - damage)
-        updateMonsterHPLabel(monsterHPLabel, currentMonster)
-        
-        if (currentMonster.CurrentHP <= 0){
-            handleDeath(currentMonster)
-        }
-        
-        let newSize = (2.0/3.0) * (damage / maxDamage) * maxLabelSize
-        let damageLabel = SKLabelNode()
-        damageLabel.fontName = "GillSans-UltraBold"
-        damageLabel.text = String(damage)
-        damageLabel.fontSize = CGFloat(newSize)
-        damageLabel.fontColor = SKColor.yellow
-      
-        //let moveToAction = SKAction.move(to: sceneLocation, duration: 1)
-        damageLabel.position = tapLocation
-        self.addChild(damageLabel)
+    func animateDamageLabel(_ label: SKLabelNode){
         let moveByAction = SKAction.moveBy(
             x: 0,
             y: 100,
             duration: 0.25
         )
         
-        let rotateByAction = SKAction.rotate(
-            byAngle: 20,
-            duration: 0.5
-        )
-        let rotateByReversedAction = rotateByAction.reversed()
-        
         let fadeinAction = SKAction.fadeIn(withDuration: 0.5)
         let fadeoutAction = SKAction.fadeOut(withDuration: 0.5)
         
         // Reversed action
         let moveByReversedAction = moveByAction.reversed()
+        
+        // action sequence
         let moveByActions = [SKAction.group([fadeinAction, moveByAction]), SKAction.group([moveByReversedAction, fadeoutAction])]
         let moveSequence = SKAction.sequence(moveByActions)
         let moveRepeatSequence = SKAction.repeat(moveSequence, count: 1)
-        damageLabel.run(moveRepeatSequence)
-        playSound(sound: currentMonster.DamageSound)
         
-        currentMonster.Node.run(currentMonster.DamageAnimation)
-        //monsterNode.run(SKAction.animate(with: Array([SKTexture.init(imageNamed: "mushroom/f_hit.png")]), timePerFrame: 0.3 ))
-        //monsterNode.run(SKAction.repeatForever(defaultMonsterAnimation))
-
-        //damageLabel.removeFromParent()
+        label.run(moveRepeatSequence)
+    }
+    
+    func handleHit(_ monster: Monster, _ tapPoint: CGPoint){
+        let damageLabel = SKLabelNode()
+        damageLabel.fontName = "Avenir"
+        damageLabel.fontSize = CGFloat(defaultFontSize)
+        damageLabel.fontColor = SKColor.yellow
+        damageLabel.position = tapPoint
+        
+        let hit = monster.HitBox.contains(tapPoint)
+        
+        print(hit)
+        print(monster.HitBox)
+        
+        if (hit){
+            var damage = Double.random(in: minDamage ... maxDamage)
+            damage.round()
+            currentMonster.CurrentHP = max(0, currentMonster.CurrentHP - damage)
+            updateMonsterHPLabel(monsterHPLabel, currentMonster)
+            
+            if (currentMonster.CurrentHP <= 0){
+                handleDeath(currentMonster)
+            }
+            
+            let newSize = (2.0/3.0) * (damage / maxDamage) * maxLabelSize
+            damageLabel.text = String(damage)
+            damageLabel.fontSize = CGFloat(newSize)
+            
+            playSound(sound: currentMonster.DamageSound)
+            
+            currentMonster.Node.run(currentMonster.DamageAnimation)
+        } else {
+            damageLabel.text = "MISS"
+        }
+        
+        animateDamageLabel(damageLabel)
+        
+        self.addChild(damageLabel)
+    }
+    
+    @objc func tap (recognizer: UIGestureRecognizer){
+        let viewLocation = recognizer.location(in: view)
+        let tapLocation = convertPoint(fromView: viewLocation)
+        handleHit(currentMonster, tapLocation)
     }
 }
---
+++ MapleRage/Models/Monster.swift
@@ -0,0 +1,95 @@
+//
+//  Monster.swift
+//  CrossoverBattles
+//
+//  Created by Patrick Lai on 2019-01-27.
+//  Copyright © 2019 Patrick Lai. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Monster{
+    var MaxHP: Double
+    var CurrentHP: Double
+    var Node: SKSpriteNode
+    var DefaultAnimation: SKAction
+    var DamageAnimation: SKAction
+    var DeathAnimation: SKAction
+    var DamageSound: SKAction
+    var DeathSound: SKAction
+    var HitBox: CGRect
+    var HasVerticalMovement: Bool
+    
+    var Width: CGFloat
+    var Height: CGFloat
+    
+    var Position: CGPoint{
+        willSet{
+            Node.position = newValue
+            HitBox.origin = CGPoint(x: newValue.x - Node.size.width / 2, y: newValue.y - Node.size.height / 2)
+        }
+    }
+    
+    init(){
+        self.MaxHP = 0
+        self.CurrentHP = 0
+        self.DefaultAnimation = SKAction()
+        self.DamageAnimation = SKAction()
+        self.DeathAnimation = SKAction()
+        self.DamageSound = SKAction()
+        self.DeathSound = SKAction()
+        self.Node = SKSpriteNode()
+        self.HitBox = CGRect()
+        self.Position = CGPoint()
+        self.Width = 0
+        self.Height = 0
+        self.HasVerticalMovement = false
+    }
+    
+    init (
+        _ MaxHP: Double,
+        _ StartingFrame: String,
+        _ DefaultAnimation: SKAction,
+        _ DamageAnimation: SKAction,
+        _ DeathAnimation: SKAction,
+        _ DamageSound: SKAction,
+        _ DeathSound: SKAction,
+        _ hasVerticalMovement: Bool = false)
+    {
+        self.MaxHP = MaxHP
+        self.CurrentHP = MaxHP
+        self.DefaultAnimation = DefaultAnimation
+        self.DamageAnimation = DamageAnimation
+        self.DeathAnimation = DeathAnimation
+        self.DamageSound = DamageSound
+        self.DeathSound = DeathSound
+        self.Node = SKSpriteNode(imageNamed: StartingFrame)
+        self.HasVerticalMovement = hasVerticalMovement
+        
+        self.Width = self.Node.size.width
+        self.Height = self.Node.size.height
+        self.Position = CGPoint(x: 0, y: 0)
+        
+        self.HitBox = CGRect(x: 0, y: 0, width: self.Width, height: self.Height)
+    }
+    
+    public static func getDefaultAnimation(_ prefix: String, _ suffix: String, _ numberOfFrames: Int, _ frameDuration: Double) -> SKAction{
+        var frames: [SKTexture] = Array()
+        for i in 0 ..< numberOfFrames {
+            let current = SKTexture.init(imageNamed: prefix + String(i) + suffix)
+            frames.append(current)
+        }
+        
+        return SKAction.animate(with: frames, timePerFrame: frameDuration)
+    }
+    
+    public static func getSpecialAnimation(_ spritePath: String, _ frameDuration: Double) -> SKAction {
+        return SKAction.animate(with: Array([SKTexture.init(imageNamed: spritePath)]), timePerFrame: frameDuration )
+    }
+    
+    public static func getSoundClip(_ soundFilePath: String, _ wait: Bool = false) -> SKAction {
+        return SKAction.playSoundFileNamed(soundFilePath, waitForCompletion: wait)
+    }
+}
+
